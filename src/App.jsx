054fb16d757bb138384662eab3b875fa4c34b1e2
NTN1789 <natanalmeida040@gmail.com>
2023-05-08 15:19:22 -0300

    arrumando largura

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import Header from "./components/Header/Header"
 import Main from "./components/Main/Main"
+import Movie from "./components/Movie/Movie"
 import Footer from "./components/footer/Footer"
 
 
@@ -21,6 +22,7 @@ function App() {
      <Header/>
         <Main/>
     <Footer/>
+
   
     </>
   )
---
+++ src/components/Movie/Movie.jsx
@@ -0,0 +1,17 @@
+import React , {useState,useEffect, useRef}from 'react'
+import { ContainerMain } from '../Main/MainStyles'
+
+const Movie = () => {
+  return (
+<>
+<ContainerMain>
+
+
+</ContainerMain>
+
+
+</>
+  )
+}
+
+export default Movie
